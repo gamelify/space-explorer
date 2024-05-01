b464f4021b1ad6c6c5b049c324d952af951f7aaf
omnicreor <fatihturker35@gmail.com>
2024-05-01 11:44:29 +0300

    Initial Commit

---
+++ src/game.js
@@ -0,0 +1,33 @@
+import Phaser from 'phaser';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+    // Load images and assets
+}
+
+function create() {
+    // Create sprites and objects
+}
+
+function update() {
+    // Game loop
+}
